1031a90d3b5c3c5a912a7e846229c99b876b7102
Jill Stapleton <jillstapleton@yahoo.com>
2016-07-06 22:41:39 -0700

    Add files via upload
    After first review, fixed variable names to match specs, fixed where/how things are displayed to match specs, added multiple projects, multiple pictures to projects, and created loop to iterate through and display pictures,

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -6,11 +6,11 @@ var bio = {
     "contacts": {
         "mobile": "510-499-xxxx",
         "email": "JillStapleton@yahoo.com",
-        "github": "I have it, but I don't really get how to use it yet",
+        "github": "https://github.com/JillStapleton/",
         "location": "Hayward, CA",
     },
+    "welcomeMessage": "Hi, thanks for looking at my resume.",
     "biopic": "images/me.jpg",
-    "welcomeMSG": "Hi, thanks for looking at my resume.",
     "skills": ["systems thinking", " learning new things", " playing well with others"]
 };
 
@@ -18,23 +18,24 @@ bio.display = function() {
     //Replace data in helper.js file with data from the bio object
     var bio_name = HTMLheaderName.replace("%data%", bio.name);
     var bio_role = HTMLheaderRole.replace("%data%", bio.role);
-    var bio_welcomMSG = HTMLwelcomeMsg.replace("%data%", bio.welcomeMSG);
+    var bio_welcomMessage = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
     var bio_mobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
     var bio_email = HTMLemail.replace("%data%", bio.contacts.email);
     var bio_github = HTMLgithub.replace("%data%", bio.contacts.github);
     var bio_location = HTMLlocation.replace("%data%", bio.contacts.location);
     var bio_pic = HTMLbioPic.replace("%data%", bio.biopic);
 
-    //This concatenation of vriables makes the placement much easier
-    var bio_lower_all = bio_pic + bio_welcomMSG + bio_mobile + bio_email + bio_github + bio_location;
-
-
     //Place data on the page: prepend for info that goes above the line
     $("#header").prepend(bio_role);
     $("#header").prepend(bio_name);
 
     //Place data on the page: append for info that goes below the line
-    $("#header").append(bio_lower_all);
+    $("#header").append(bio_pic);
+    $("#header").append(bio_welcomMessage);
+    $("#topContacts, #footerContacts").append(bio_mobile);
+    $("#topContacts, #footerContacts").append(bio_email);
+    $("#topContacts, #footerContacts").append(bio_github);
+    $("#topContacts, #footerContacts").append(bio_location);
 
     //look for skills in the bio object, and if present put them under the Skills header
     if (bio.skills.length > 0) {
@@ -70,11 +71,10 @@ var work = {
 };
 
 work.display = function() {
-    $("#workExperience").append(HTMLworkStart);
-    //Place header
     for (var job = 0; job < work.jobs.length; job++) {
+        //Place header
+        $("#workExperience").append(HTMLworkStart);
         //Replace data in helper.js variables with data from the work object
-
         var work_employer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
         var work_title = HTMLworkTitle.replace("%data%", work.jobs[job].title);
         var work_dates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
@@ -90,30 +90,44 @@ work.display();
 //-----------------------------------------------the projects section-----------------------------------
 var projects = {
     //Define project object
-    "project": [{
-            "title": "Bobbin Lace",
-            "dates": "1997-2000",
-            "description": "I had a brief obsession with bobbin lace. It seems that the more detailed" +
-                " a craft is, the more I like it. I believe this bodes well for programming and it's" +
-                " blasted semicolons and commas.",
-            "image": ["images/bobbin.jpg"]
-        }]
-        //display()
+    "projects": [{
+        "title": "Bobbin Lace",
+        "dates": "1997-2000",
+        "description": "I had a brief obsession with bobbin lace. It seems that the more detailed" +
+            " a craft is, the more I like it. I believe this bodes well for programming and it's" +
+            " blasted semicolons and commas.",
+        "images": ["images/bobbin.jpg", "images/bobbin2.jpg"]
+    }, {
+        "title": "Tatting",
+        "dates": "1997-2010",
+        "description": "I had a longer period of working with tatting (it takes way less room).",
+        "images": ["images/tatting.jpg", "images/tatting2.jpg"]
+    }, {
+        "title": "Parenting",
+        "dates": "2011 and beyond",
+        "description": "Just in case you're wondering why I stopped my ultra cool hobbies... Well, there were babies.",
+        "images": ["images/baby.jpg", "images/kids.jpg"]
+    }]
 };
 
 projects.display = function() {
-    $("#projects").append(HTMLprojectStart);
-    //Place header
-    for (var item = 0; item < projects.project.length; item++) {
+    for (var item = 0; item < projects.projects.length; item++) {
+        //Place header
+        $("#projects").append(HTMLprojectStart);
         //Replace data in helper.js variables with data from the projects object
-        var projects_title = HTMLprojectTitle.replace("%data%", projects.project[item].title);
-        var projects_dates = HTMLprojectDates.replace("%data%", projects.project[item].dates);
-        var projects_description = HTMLprojectDescription.replace("%data%", projects.project[item].description);
-        var projects_image = HTMLprojectImage.replace("%data%", projects.project[item].image);
-        var projects_all = projects_title + projects_dates + projects_description + projects_image;
-        //Place data on the page: append for info that goes below the line
+        var projects_title = HTMLprojectTitle.replace("%data%", projects.projects[item].title);
+        var projects_dates = HTMLprojectDates.replace("%data%", projects.projects[item].dates);
+        var projects_description = HTMLprojectDescription.replace("%data%", projects.projects[item].description);
+        var projects_all = projects_title + projects_dates + projects_description;
+        // place project info on the page
         $(".project-entry:last").append(projects_all);
+        //replace data in HTMLprojectImage with each appropriate image, place them on the page
+        for (var pic = 0; pic < projects.projects[item].images.length; pic++) {
+            var projects_images = HTMLprojectImage.replace("%data%", projects.projects[item].images[pic]);
+            $(".project-entry:last").append(projects_images);
+        }
     }
+
 };
 projects.display();
 
@@ -124,32 +138,33 @@ var education = {
         "name": "University of San Francisco",
         "location": "San Francisco, CA",
         "degree": "BA",
-        "majors": "Urban Studies",
+        "majors": ["Urban Studies"],
         "dates": "2000",
         "url": "http://www.sfsu.edu/"
     }],
     "onlineCourses": [{
-        "onlineTitle": "Intro to Programming",
-        "onlineSchool": "Udacity",
-        "onlineDates": "2016",
-        "onlineURL": "www.udacity.com"
+        "Title": "Intro to Programming",
+        "School": "Udacity",
+        "Dates": "2016",
+        "URL": "www.udacity.com"
     }, {
-        "onlineTitle": "Front-End Web Developer ",
-        "onlineSchool": "Udacity",
-        "onlineDates": "Expected completion: 2016",
-        "onlineURL": "www.udacity.com"
+        "Title": "Front-End Web Developer ",
+        "School": "Udacity",
+        "Dates": "Expected completion: 2016",
+        "URL": "www.udacity.com"
     }, {
-        "onlineTitle": "Full Stack Web Developer",
-        "onlineSchool": "Udacity",
-        "onlineDates": "Expected completion: 2017",
-        "onlineURL": "www.udacity.com"
+        "Title": "Full Stack Web Developer",
+        "School": "Udacity",
+        "Dates": "Expected completion: 2017",
+        "URL": "www.udacity.com"
     }]
 };
 
 education.display = function() {
-    $("#education").append(HTMLschoolStart);
-    //Place header
     for (var schooled = 0; schooled < education.schools.length; schooled++) {
+        //Place header
+        $("#education").append(HTMLschoolStart);
+
         //Replace data in helper.js variables with data from the education object
         var education_name = HTMLschoolName.replace("%data%", education.schools[schooled].name);
         var education_degree = HTMLschoolDegree.replace("%data%", education.schools[schooled].degree);
@@ -158,21 +173,20 @@ education.display = function() {
         var education_major = HTMLschoolMajor.replace("%data%", education.schools[schooled].majors);
         var education_all = education_name + education_degree + education_dates + education_location + education_major;
         //Place data on the page: append for info that goes below the line
-        $(".education-entry").append(education_all);
+        $(".education-entry:last").append(education_all);
     }
-
-
-    $(".education-entry").append(HTMLonlineClasses);
-    //Place header
     for (var onLine = 0; onLine < education.onlineCourses.length; onLine++) {
+        //Place header
+        $("#education").append(HTMLonlineClasses);
+        $("#education").append(HTMLschoolStart);
         //Replace data in helper.js variables with data from the onLineEd object
-        var onLineEd_title = HTMLonlineTitle.replace("%data%", education.onlineCourses[onLine].onlineTitle);
-        var onLineEd_school = HTMLonlineSchool.replace("%data%", education.onlineCourses[onLine].onlineSchool);
-        var onLineEd_dates = HTMLonlineDates.replace("%data%", education.onlineCourses[onLine].onlineDates);
-        var onLineEd_url = HTMLonlineURL.replace("%data%", education.onlineCourses[onLine].onlineURL);
+        var onLineEd_title = HTMLonlineTitle.replace("%data%", education.onlineCourses[onLine].Title);
+        var onLineEd_school = HTMLonlineSchool.replace("%data%", education.onlineCourses[onLine].School);
+        var onLineEd_dates = HTMLonlineDates.replace("%data%", education.onlineCourses[onLine].Dates);
+        var onLineEd_url = HTMLonlineURL.replace("%data%", education.onlineCourses[onLine].URL);
         var onLineEd_all = onLineEd_title + onLineEd_school + onLineEd_dates + onLineEd_url;
         //Place data on the page: append for info that goes below the line
-        $(".education-entry").append(onLineEd_all);
+        $(".education-entry:last").append(onLineEd_all);
 
     }
 };
